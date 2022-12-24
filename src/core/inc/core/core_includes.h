69e6c9bcd87cf8a44952a3bdb8873b0aeddabe54
sengulhamza <sengulhamza@icloud.com>
2022-12-24 22:37:27 +0300

    base project added

---
+++ src/core/inc/core/core_includes.h
@@ -0,0 +1,6 @@
+#ifndef _CORE_INCLUDES_H_
+#define _CORE_INCLUDES_H_
+
+#include "core/core_tasks.h"
+
+#endif
---
+++ src/core/inc/core/core_tasks.h
@@ -0,0 +1,21 @@
+
+#ifndef _CORE_TASKS_H_
+#define _CORE_TASKS_H_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOSConfig.h"
+
+#define KBYTE (1024)
+
+#define CORE_TASK_PRIO_MAX           (configMAX_PRIORITIES)
+#define CORE_TASK_PRIO_MIN           (0)
+#define CORE_TASK_MIN_STACK          (configMINIMAL_STACK_SIZE)
+#define CORE_TASK_STACK_TYPE         portSTACK_TYPE
+#define CORE_TASK_SIZE(a)            (sizeof(CORE_TASK_STACK_TYPE)*(a))
+
+
+#define CORE_LVGL_ROUTUNE_TASK_PRIO         (CORE_TASK_PRIO_MIN + 6)
+#define CORE_LVGL_ROUTUNE_TASK_STACK        (4*KBYTE + CORE_TASK_MIN_STACK)
+#define CORE_LVGL_ROUTUNE_TASK_NAME         "lvgl_routune_timer"
+
+#endif
